77802fdce31265c4ccf8943fe6c8463b9276e217
Maciej Szaflik <maciej.szaflik@gmail.com>
2019-12-07 18:11:20 +0100

    adding works now

--- main/Board/matrix.lua
+++ main/Board/matrix.lua
@@ -1,9 +1,6 @@
-
-
 Matrix = {}
 Matrix.mt = {}
-Matrix.mt.__add = Matrix.add
-
+Matrix.mt.__add = Matrix.addSimple
 
 function Matrix.new(n,m)
 	local mt = {}
@@ -17,16 +14,18 @@ function Matrix.new(n,m)
 	return mt
 end
 
-function Matrix.add(m1,m2)
-	local toReturn = {}
-	for i=1,n do
-		toReturn = {}
-		for j=1,m do
+function Matrix.addSimple(m1,m2)
+	local toReturn = Matrix.new(table.getn(m1),table.getn(m1[1]))
+	
+	for i=1,table.getn(m1) do
+		toReturn[i] = {}
+		for j=1,table.getn(m1[i]) do
 			toReturn[i][j] = m1[i][j] + m2[i][j]
 		end
 	end
 	return toReturn
 end
+Matrix.mt.__add = Matrix.addSimple
 
 function Matrix.append(m1,m2)
 	for i=1,n do
